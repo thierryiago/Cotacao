28739d975928ebbe30baa087be5fe2c07e8cbcfc
Thierry <thierryiago@gmail.com>
2023-01-16 19:44:18 -0300

    Adicionando os exercícios

---
+++ Exercícios/Program.cs
@@ -0,0 +1,41 @@
+﻿
+#region Exercício 1
+Console.WriteLine("Exercício 1:");
+
+int i, n, soma = 0;
+Console.WriteLine("Entre com um número :");
+n = Convert.ToInt32(Console.ReadLine());
+if (n > 0)
+{
+    for (i = 1; i <= n; i++)
+    {
+        soma += i;
+    }
+
+    Console.WriteLine("Soma: " + soma);
+}
+else
+    Console.WriteLine("O número precisa ser maior que 0");
+
+
+#endregion
+
+
+
+#region Exercício 2
+Console.WriteLine("Exercício 2:");
+
+List<string> lista = new List<string> { "abracadabra", "allottee", "assessee" };
+
+foreach (var item in lista)
+{
+    Console.WriteLine("\n String inicial: " + item);
+    Console.Write("String sem duplicações: ");
+    var uniqueCharArray = item.ToCharArray().Distinct().ToArray();
+    var resultString = new string(uniqueCharArray);
+
+    Console.Write(resultString);
+}
+
+#endregion
+
